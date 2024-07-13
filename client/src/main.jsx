8adffbd29bd2ffa4d58e643b36b4d69842ef0aa8
Rijo Sebastian <rijobdk@gmail.com>
2024-07-13 21:46:49 +0530

    feat: Add CORS support, integrate signup/login API, and enhance UI with toast messages
    - Added CORS support in the server to handle cross-origin requests.
    - Integrated API endpoints for user signup and login.
    - Implemented toast messages for better user feedback and improved UI/UX.
    - Updated Login and Signup components with form validation using Yup and React Hook Form.
    - Enhanced Hero component and ThemeSwitch module for better styling.
    - Configured Axios for API requests.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { MantineProvider } from '@mantine/core';
-import './index.css'
-import '@mantine/core/styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { MantineProvider } from "@mantine/core";
+import "./index.css";
+import "@mantine/core/styles.css";
+import { Toaster } from "react-hot-toast";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MantineProvider>
       <App />
+
+    
+      <Toaster position="bottom-center"  duration={500} />
     </MantineProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
