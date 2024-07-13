49dadc025b96a324e0646b51ca733f1fdf9178f9
Rijo Sebastian <rijobdk@gmail.com>
2024-07-13 17:50:17 +0530

    feat: Add form validation using Yup and React Hook Form
    - Implemented email validation in Login and Signup components using Yup schema.
    - Integrated React Hook Form for managing form state and validation.
    - Enhanced user experience with improved error handling and validation feedback.

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -9,9 +9,36 @@ import {
   Stack,
   Flex,
 } from "@mantine/core";
+
 import { Link } from "react-router-dom";
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  name: yup.string().required("Enter your name"),
+  email: yup.string().required("Enter your email").matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/gm, "Enter a valid email"),
+  password: yup
+    .string()
+    .required()
+    .min(6, "Password must be at least 6 characters"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match"),
+});
 export default function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
+
   return (
     <Flex
       gap="xs"
@@ -28,28 +55,37 @@ export default function SignUp() {
         <Text size="xl" fw={600} mb={10} c="blue" ta={"center"}>
           Sign Up
         </Text>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Stack>
-            <TextInput label="Name" placeholder="Your name" radius="xl" />
+            <TextInput
+              label="Name"
+              placeholder="Your name"
+              radius="xl"
+              {...register("name")}
+              error={errors.name && errors.name.message}
+            />
 
             <TextInput
-              required
               label="Email"
               placeholder=" Your email"
               radius="xl"
+              {...register("email")}
+              error={errors.email && errors.email.message}
             />
 
             <PasswordInput
-              required
               label="Password"
               placeholder="Your password"
               radius="xl"
+              {...register("password")}
+              error={errors.password && errors.password.message}
             />
             <PasswordInput
-              required
               label="Confirm Password"
               placeholder="Confirm your password"
               radius="xl"
+              {...register("confirmPassword")}
+              error={errors.confirmPassword && errors.confirmPassword.message}
             />
           </Stack>
 
