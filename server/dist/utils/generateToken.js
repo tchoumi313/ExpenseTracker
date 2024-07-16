8aa44e9d25d7ad2d83d8401cff893bf9225b454f
gurudaspr <gurudaspranavam007@gmail.com>
2024-07-16 16:06:50 +0530

    added token options

--- server/dist/utils/generateToken.js
+++ server/dist/utils/generateToken.js
@@ -10,7 +10,7 @@ const generateToken = (userId, res) => {
         maxAge: 15 * 24 * 60 * 60 * 1000, // MS
         httpOnly: true,
         secure: true,
-        sameSite: 'none',
+        sameSite: 'lax',
     });
     return token;
 };
